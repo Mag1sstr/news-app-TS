ac33a395b4682eb9c16df9cec7a1e2e788254462
Mag1sstr <danilkarachev234@mail.ru>
2024-12-08 20:11:59 +0500

    Update Main.tsx

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import NewsBanner from "../../components/NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
-import { getNews } from "../../api/apiNews";
+import { getCategories, getNews } from "../../api/apiNews";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
 import Pagination from "../../components/Pagination/Pagination";
+import Categories from "../../components/Categories/Categories";
 
 export default function Main() {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const totalPage = 10;
   const pageSize = 10;
   const fetchNews = async (currentPage: number) => {
     try {
       setIsLoading(true);
-      const response = await getNews(currentPage, pageSize);
+      const response = await getNews({
+        page_number: currentPage,
+        page_size: pageSize,
+        category: selectedCategory === "All" ? null : selectedCategory,
+      });
       console.log(response);
       setNews(response.news);
       setIsLoading(false);
@@ -23,9 +30,21 @@ export default function Main() {
       console.log(error);
     }
   };
+  const fetchCategories = async () => {
+    try {
+      const response = await getCategories();
+      console.log(response);
+      setCategories(["All", ...response.categories]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchCategories();
+  }, []);
   useEffect(() => {
     fetchNews(currentPage);
-  }, [currentPage]);
+  }, [currentPage, selectedCategory]);
 
   const handleNextPage = () => {
     if (currentPage < totalPage) {
@@ -42,6 +61,12 @@ export default function Main() {
   };
   return (
     <main className={styles.main}>
+      <Categories
+        categories={categories}
+        setSelectedCategory={setSelectedCategory}
+        selectedCategory={selectedCategory}
+      />
+
       {news.length && !isLoading ? (
         <NewsBanner item={news[0]} />
       ) : (
