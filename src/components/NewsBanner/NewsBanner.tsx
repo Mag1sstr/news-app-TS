a32b81358ae0f5017cacf4532cb4d8ff1fc24fa1
Mag1sstr <danilkarachev234@mail.ru>
2024-12-10 17:58:47 +0500

    Update NewsBanner.tsx

--- src/components/NewsBanner/NewsBanner.tsx
+++ src/components/NewsBanner/NewsBanner.tsx
@@ -1,4 +1,5 @@
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
+import withSkeleton from "../../helpers/hocs/withSkeleton";
 import { INews } from "../../interfaces/interfaces";
 import Image from "../Image/Image";
 import styles from "./styles.module.css";
@@ -6,14 +7,18 @@ import styles from "./styles.module.css";
 interface IProps {
   item: INews;
 }
-export default function NewsBanner({ item }: IProps) {
+function NewsBanner({ item }: IProps) {
   return (
     <div className={styles.banner}>
-      <Image image={item?.image} />
-      <h3 className={styles.title}>{item.title}</h3>
+      <Image image={item.image} />
+      <h3 className={styles.title}>{item?.title}</h3>
       <p className={styles.extra}>
         {formatTimeAgo(item.published)} by {item.author}
       </p>
     </div>
   );
 }
+
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
