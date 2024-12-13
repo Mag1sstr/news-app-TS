1088c486661c3d480219329314eb73764ef3a5e0
Mag1sstr <danilkarachev234@mail.ru>
2024-12-13 23:15:24 +0500

    Create LatestNews.tsx

---
+++ src/components/LatestNews/LatestNews.tsx
@@ -0,0 +1,14 @@
+import { getLatestNews } from "../../api/apiNews";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import BannersList from "../BannersList/BannersList";
+import styles from "./styles.module.css";
+
+export default function LatestNews() {
+  const { data, isLoading }: any = useFetch(getLatestNews, {});
+
+  return (
+    <section className={styles.section}>
+      <BannersList banners={data && data.news} isLoading={isLoading} />
+    </section>
+  );
+}
