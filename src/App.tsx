2407bc1e4ba5f79ee2b34e1d7b2a217061302529
Mag1sstr <danilkarachev234@mail.ru>
2024-12-17 16:43:20 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
   return (
     <>
       <Header />
-      <div className="conteiner">
+      <div className={`conteiner ${context?.isDark ? "dark" : null}`}>
         <Main />
       </div>
     </>
