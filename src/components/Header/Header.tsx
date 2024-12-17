83ce55ce36b2c9237a3a865587e1c6c6bd1fd03a
Mag1sstr <danilkarachev234@mail.ru>
2024-12-17 16:43:28 +0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
+import { useContext } from "react";
 import { formatDate } from "../../helpers/formatDate";
 import styles from "./styles.module.css";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 export default function Header() {
+  const context = useContext(ThemeContext);
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>NEWS APP</h1>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+    <header
+      className={`${styles.header} ${context?.isDark ? styles.dark : null}`}
+    >
+      <div>
+        <h1 className={styles.title}>NEWS APP</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+      </div>
+      <div
+        onClick={() => {
+          context?.toggleTheme();
+        }}
+        className={`${styles.button} ${context?.isDark ? styles.active : null}`}
+      >
+        <div className={styles.circle}></div>
+      </div>
     </header>
   );
 }
