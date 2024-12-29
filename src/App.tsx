8b868f046bfb27ad3a137b5a7734e274a5623256
Mag1sstr <danilkarachev234@mail.ru>
2024-12-29 19:36:10 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { useContext } from "react";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NewsPage from "./components/NewsPage/NewsPage";
 
 function App() {
   const context = useContext(ThemeContext);
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className={`conteiner ${context?.isDark ? "dark" : null}`}>
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/news/:id" element={<NewsPage />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
