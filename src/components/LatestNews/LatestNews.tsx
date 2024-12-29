f7bc012f066e2a4dfcfc0626e707a78aea9d9cad
Mag1sstr <danilkarachev234@mail.ru>
2024-12-29 19:36:21 +0500

    Update LatestNews.tsx

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,14 +1,22 @@
-import { getLatestNews } from "../../api/apiNews";
+// import { getLatestNews } from "../../api/apiNews";
+import { useEffect } from "react";
 import { useFetch } from "../../helpers/hooks/useFetch";
+import { getLatestNews } from "../../store/newsSlice";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import BannersList from "../BannersList/BannersList";
 import styles from "./styles.module.css";
 
 export default function LatestNews() {
-  const { data, isLoading }: any = useFetch(getLatestNews, {});
+  // const { data, isLoading }: any = useFetch(getLatestNews, {});
+  const news = useAppSelector((state) => state.news.latestNews);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getLatestNews());
+  }, []);
 
   return (
     <section className={styles.section}>
-      <BannersList banners={data && data.news} isLoading={isLoading} />
+      <BannersList banners={news} isLoading={!news.length} />
     </section>
   );
 }
